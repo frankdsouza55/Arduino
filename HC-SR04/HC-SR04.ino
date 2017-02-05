e0ea3bcee072aaa16db47bc6ab5cb0923fa80b98
frankdsouza55 <frankdsouza55@gmail.com>
2017-02-05 20:56:22 +0530

    Hc-SR04 Sensor
    Make use of the ultra sonic sensor

---
+++ HC-SR04/HC-SR04.ino
@@ -0,0 +1,35 @@
+int trig=13;
+int echo=12;
+int sig1=10;
+int sig2=9;
+
+void setup()
+{
+  Serial.begin (9600);
+  pinMode(trig,OUTPUT);
+  pinMode(echo,INPUT);
+  pinMode(sig1,OUTPUT);
+  pinMode(sig2,OUTPUT);
+}
+
+void loop()
+{
+  long duration, distance;
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  duration = pulseIn(echo,HIGH);
+  distance = (duration/2)/29.1;
+  if (distance<15){ 
+    digitalWrite(sig2, LOW);
+    digitalWrite(sig1, HIGH);
+  }
+  else { 
+    digitalWrite(sig1, LOW);
+    digitalWrite(sig2, HIGH);
+    }
+  
+}
+
